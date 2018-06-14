000bbf68823e065f950e5d54b49b1a7901743f40
PatWie <mail@patwie.com>
2018-06-14 18:35:44 +0200

    fix ci

---
+++ .ci/build.sh
@@ -0,0 +1,12 @@
+export TensorFlow_GIT_REPO=/tensorflow
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs:${LD_LIBRARY_PATH}
+
+cd inference/cc/
+cp ../../.ci/tensorflow_config.example.txt tensorflow_config.txt
+cmake .
+make
+
+cd ../c/
+cp ../../.ci/tensorflow_config.example.txt tensorflow_config.txt
+cmake .
+make
