3de63fd27a1f4173a6f4cb868b90e178a2e83425
PatWie <mail@patwie.com>
2018-07-29 10:56:44 +0200

    Add build test for go-inference

--- inference/go/build.sh
+++ inference/go/build.sh
@@ -1,5 +1,3 @@
-export LD_LIBRARY_PATH=${TENSORFLOW_BUILD_DIR}/:${LD_LIBRARY_PATH}
-export LIBRARY_PATH=${TENSORFLOW_BUILD_DIR}/:${LIBRARY_PATH}
 TF_PATH=`python -c "import tensorflow as tf; print(tf.__path__[0])"`
 export LD_LIBRARY_PATH=${TF_PATH}:${LD_LIBRARY_PATH}
 export LIBRARY_PATH=${TF_PATH}:${LIBRARY_PATH}
