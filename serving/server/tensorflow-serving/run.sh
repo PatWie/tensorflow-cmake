73227fc0e09a95e19dd422f85570a8435ebcd024
PatWie <mail@patwie.com>
2018-08-29 18:25:36 +0200

    remove duplicates

--- serving/server/tensorflow-serving/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-  -p 8501:8501 \
-  -p 8500:8500 \
-  --mount type=bind,source=/tmp/simple_model,target=/models/simple_model \
-  -e MODEL_NAME=simple_model -t tensorflow/serving
