9d3885f1e97b2dff83b8afced40fc3b82fa1a0b2
PatWie <mail@patwie.com>
2018-10-02 13:39:10 +0200

    add 1.11

--- cmake/modules/FindTensorFlow.cmake
+++ cmake/modules/FindTensorFlow.cmake
@@ -122,7 +122,7 @@ else()
     # However, only TensorFlow versions 1.9, 1.10 support all header files
     # for custom ops.
     set(_TensorFlow_KNOWN_VERSIONS ${TensorFlow_ADDITIONAL_VERSIONS}
-        "1.9" "1.9.0" "1.10" "1.10.0")
+        "1.9" "1.9.0" "1.10" "1.10.0", "1.11", "1.11.0")
     set(_TensorFlow_TEST_VERSIONS)
 
     if(TF_FIND_VERSION)
